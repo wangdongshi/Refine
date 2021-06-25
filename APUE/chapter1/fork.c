1e6c455b1cc3f1c6c8858149e6ad56fbd6f0d8bc
WangYu <yu.wang@cn.yokogawa.com>
2021-06-25 15:16:24 +0800

    Modify fork.c. However, fork.c can not be compiled correctly.

--- APUE/chapter1/fork.c
+++ APUE/chapter1/fork.c
@@ -16,7 +16,7 @@ int main(void)
 			err_sys("fork error");
 		} else if (pid == 0) {	/* child */
 			execlp(buf, buf, (char *)0);
-			err_ret("couldn't execute: %s"), buf;
+			err_ret("couldn't execute: %s", buf);
 			exit(127);
 		}
 		/* parent */
